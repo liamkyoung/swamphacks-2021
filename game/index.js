50e5ad82a731dffac0150e8cd91c41ee77d02fbe
Nikola Pavlicek <npavlicek@users.noreply.github.com>
2021-01-30 01:07:30 -0500

    documentation

--- game/index.js
+++ game/index.js
@@ -1,14 +1,18 @@
+// Determine if the web browser supports WebGL
 let type = 'WebGL'
 if (!PIXI.utils.isWebGLSupported()) {
   type = 'Canvas'
 }
 PIXI.utils.sayHello(type)
 
+// Set up the PIXI app and add it to the html document as a Canvas
 let app = new PIXI.Application({width: 1280, height: 720, antialias: true})
 document.body.appendChild(app.view)
 
+// Object to contain all the sprites
 let sprites = {}
 
+// Load the textures and create new sprites in the sprites Object
 const loader = PIXI.Loader.shared
 loader.add('cat', 'res/cat.png')
 loader.load((loader, res) => {
