ef7fb0dd6ac121c09651f620093d05c34d5f7278
Nikola Pavlicek <npavlicek@users.noreply.github.com>
2021-01-30 00:14:31 -0500

    added first sprite

--- game/index.js
+++ game/index.js
@@ -2,9 +2,16 @@ let type = 'WebGL'
 if (!PIXI.utils.isWebGLSupported()) {
   type = 'Canvas'
 }
-
 PIXI.utils.sayHello(type)
 
-let app = new PIXI.Application({width: 1280, height: 720})
-
+let app = new PIXI.Application({width: 1280, height: 720, antialias: true})
 document.body.appendChild(app.view)
+
+let sprites = {}
+
+const loader = PIXI.Loader.shared
+loader.add('cat', 'res/cat.png')
+loader.load((loader, res) => {
+  sprites.cat = new PIXI.Sprite(res.cat.texture)
+  app.stage.addChild(sprites.cat)
+})
